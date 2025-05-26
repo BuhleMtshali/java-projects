4e56ad1b196a4458543bda4e7757768ba4c8e073
Buhle <mtshalizanobuhle7@gmail.com>
2025-05-26 21:58:16 +0200

    updated the variables folder

--- java-mini-projects/java-variables/Variables.java
+++ java-mini-projects/java-variables/Variables.java
@@ -1,6 +1,8 @@
 
 public class Variables {
     public static void main(String[] args){
+        //Primitive data types => store actual values
+
         //intergers whole numbers
         int numberFirst = 12;
         int numberSecond = 6;
@@ -9,5 +11,18 @@ public class Variables {
         //doubles decimal numbers
         double decimalNumber = 4.5;
         System.out.println(decimalNumber);
+
+        //char single characters
+        char grade = 'B';
+        System.out.println(grade);
+
+        //boolean true/false
+        boolean isStudent = false;
+        System.out.println(isStudent);
+
+        //reference => point to the objects stored elsewhere in memory
+        //strings
+        String name = "Buhle";
+        System.out.println(name);
     }
 }
