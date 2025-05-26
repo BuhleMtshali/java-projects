6f0e866fba951cb5014d3029ffefbddfa793ee31
Buhle <mtshalizanobuhle7@gmail.com>
2025-05-26 22:37:34 +0200

    updated the inputs file

--- java-mini-projects/java-inputs/Inputs.java
+++ java-mini-projects/java-inputs/Inputs.java
@@ -1,11 +1,31 @@
 import java.util.Scanner;
 public class Inputs {
-    public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        System.out.println("What's your name? ");
-        String name = input.nextLine();
-        System.out.println("Hello " + name);
+  public static void main(String[] args){
 
-        input.close();
+    //open the mic
+    Scanner input = new Scanner(System.in);
+
+    //printing out the name
+    System.out.print("What's your name? ");
+    String name = input.nextLine();
+
+    System.out.print("How old are you? ");
+    int age = input.nextInt();
+
+    System.out.print("How much is the bus fare? ");
+    double price = input.nextDouble();
+
+    System.out.print("Are you a student? ");
+    boolean isStudent = input.nextBoolean();
+
+    if(isStudent){
+        System.out.println("You are currently a student");
+    } else {
+        System.out.println("You are not a student");
     }
+
+    System.out.println("Hello " + " " + name + " " + "you are " + " " + age + "" + "and the price of the bus fare is R" + price + "and currently you are a  " + isStudent);
+
+    input.close();
+  }
 }
