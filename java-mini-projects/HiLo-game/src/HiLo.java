5a9a08ac08a5d8e02cbea2e6d1c3817c8eb84074
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-07 20:41:07 +0200

    updated the files

--- java-mini-projects/HiLo-game/src/HiLo.java
+++ java-mini-projects/HiLo-game/src/HiLo.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class HiLo {
     public static void main(String[] args){
-        System.out.println("Hello Buhle");
+        Scanner input = new Scanner(System.in);
+
+        int randomNumber = (int)(Math.random() * 100 + 1);
+
+        System.out.println(randomNumber);
+
     }
 }
