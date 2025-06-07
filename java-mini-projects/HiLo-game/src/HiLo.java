a29da8380ac5f0ef34cfca8a2c36856b58e73fc5
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-07 21:46:59 +0200

    updated the Hilo file

--- java-mini-projects/HiLo-game/src/HiLo.java
+++ java-mini-projects/HiLo-game/src/HiLo.java
@@ -10,28 +10,28 @@ public class HiLo {
         do {
 
             //1. we gonna generate the random number by the computer
-            int randomNumber = (int)(Math.random() * 10 + 1); //whole number between 1 and 10 both inclusive;
+            int randomNumber = (int)(Math.random() * 100 + 1); //whole number between 1 and 10 both inclusive;
 
             //2. usernumber will be the inputed number by the user 
             int usernumber = 0;
-
+            
+             System.out.println("---Welcome to HiLo Number Guessing Game👾---");
             //3. we gonna a while which will keep running untill random number = userNumber
             while (usernumber != randomNumber) {
-                System.out.println("---Welcome to HiLo Number Guessing Game👾---");
-                System.out.print("Guess a number between 1 and 10");
+                System.out.print("Guess a number between 1 and 100: ");
                 usernumber = input.nextInt(); //we're reading the user's inputed number
+                input.nextLine(); //flush the leftover newline
 
                 //4. adding conditionals to check if random </> the usernumber
                 if(usernumber < randomNumber)
-                    System.out.println("Ouch😩! Your guessed " + usernumber + " and the computer gave " + randomNumber + " try again!!! a lil higher this time!!");
+                    System.out.println("Ouch😩! Your guessed " + usernumber + " try again!!! a lil higher this time!!");
                 else if(usernumber > randomNumber)
-                    System.out.println("Damn😔 " + usernumber + " is Higher than the computer's " + randomNumber + " let's go lower this time!!");
+                    System.out.println("Damn😔 " + usernumber + " let's go lower this time!!");
                 else
-                System.out.println("Wow!! you just right on the dot!!🍾🥂");
+                System.out.println("Wow!! you just right on the dot!!🍾🥂 you guessed " + usernumber + " and the number was " + randomNumber);
             }
 
         System.out.print("Would you like to go another round (y/n)");  
-        input.nextLine();
         playAgain = input.nextLine(); //reads the reply from the user whether they wannna play again or not
 
         } while (playAgain.equalsIgnoreCase("y")); //keep looping if the reply is yes from the user
