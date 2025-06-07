4fac5a273dcb49d760e088a2cee8dd0512f96da0
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-07 20:45:22 +0200

    updated the random number variable

--- java-mini-projects/HiLo-game/src/HiLo.java
+++ java-mini-projects/HiLo-game/src/HiLo.java
@@ -4,9 +4,16 @@ public class HiLo {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
 
-        int randomNumber = (int)(Math.random() * 100 + 1);
+        int randomNumber = (int)(Math.random() * 100 + 1); //to generate a random number between 1 and 100 and ensures its a whole number
 
-        System.out.println(randomNumber);
+        int userNumber = 0;
+
+        System.out.print("Guess a number between 1 and 100: ");
+        userNumber = input.nextInt();
+
+        
+
+
 
     }
 }
