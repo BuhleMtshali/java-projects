e5736b28ff4b458effd6ea56f552b483e6b8d710
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-07 12:40:09 +0200

    Added the variable values

--- java-mini-projects/java-inputs-game/Madlibs.java
+++ java-mini-projects/java-inputs-game/Madlibs.java
@@ -4,11 +4,16 @@ public class Madlibs {
     public static void main(String[] args){
        Scanner input = new Scanner(System.in); //create the scanner variable
 
+        System.out.println("---- 👾Welcome to the School Trip Form🛣️ ------");
+
         System.out.print("What's your name? "); //asking the user the question
         String name = input.nextLine();  //storing the user's input
 
         System.out.print("What's your last name? ");
         String lastName = input.nextLine();
+
+        System.out.print("What is the name of your School? ");
+        String schoolName = input.nextLine();
 
         System.out.print("How old Are you? ");
         int age = input.nextInt();
@@ -16,8 +21,8 @@ public class Madlibs {
         System.out.print("How much have you saved so far? ");
         Double savings = input.nextDouble();
 
-        
-
+        System.out.println("Thank you for completing the " + schoolName + "'s Year End Trip form " + name + " " + lastName + ". You are currently" + age + " years old. You currently have R" + savings + " in your savings. We look forwad to hhaving you " + name + ".");
+        System.out.println("------ The END -----");
        input.close(); //close the scanner object
     }
 }
