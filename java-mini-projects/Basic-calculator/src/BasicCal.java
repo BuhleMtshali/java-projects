1461a558d96b74994b26a607ef25766ba0c9468a
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-08 06:15:01 +0200

    updated some variables in java

--- java-mini-projects/Basic-calculator/src/BasicCal.java
+++ java-mini-projects/Basic-calculator/src/BasicCal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class BasicCal {
     public static void main(String[] args){
-        System.out.println("Helo world");
+        Scanner input = new Scanner(System.in);
+        
+        //WELCOME MESSAGE
+        System.out.println("---🧮Welcome to the Basic Calculator Applocation👾---");
+
+        //1. asking the user for the first number
+        System.out.print("Enter your first number: ");
+        double num1 = input.nextDouble();
+
+        //2. asking the use to choose the operator
+        System.out.println("Enter an operator (+, -, *, /): ");
+        char operator = input.next().charAt(0);  //remember the user is returning a single charactor
+
+        System.out.println("Enter yur second number: ");
+        double num2 = input.nextDouble();
+
+
+
     }
 }
