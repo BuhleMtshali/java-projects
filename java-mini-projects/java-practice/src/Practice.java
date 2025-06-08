d96ffbb3c21e177d2287c188ff181fa8a9685ea8
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-08 13:42:02 +0200

    updated the practice folder

--- java-mini-projects/java-practice/src/Practice.java
+++ java-mini-projects/java-practice/src/Practice.java
@@ -1,7 +1,10 @@
 public class Practice {
     public static void main(String[] args){
-      //finding random numbers
+
+      //1. finding random numbers with the Math.random()
       int random = (int)(Math.random() * 10 + 1);
       System.out.println(random);
+
+       
     }
 }
