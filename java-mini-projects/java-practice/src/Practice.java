d94559753ff4c41002eae942cfbe44880a78565b
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-08 16:16:00 +0200

    updated the practice.java file

--- java-mini-projects/java-practice/src/Practice.java
+++ java-mini-projects/java-practice/src/Practice.java
@@ -2,12 +2,23 @@ public class Practice {
     public static void main(String[] args){
 
       //1. finding random numbers with the Math.random()
-      int random = (int)(Math.random() * 10 + 1);
-      System.out.println(random);
+      //int random = (int)(Math.random() * 10 + 1);
+        //System.out.println(random);
 
        //for loop
-       for (int i = 0; i < 10; i++){
-        System.out.println(" i is now: " + i);
-       }
+       //for (int i = 0; i < 10; i++){
+        //System.out.println(" i is now: " + i);
+       //}
+
+      //conditional rendering
+      String name = "Dirk";
+      int x = 5;
+
+      if (x <= 5 && name.equals("Dirk")) {
+        System.out.println("I am Dirk");
+      } else {
+        System.out.println("There is no Dirk");
+      }
+
     }
 }
