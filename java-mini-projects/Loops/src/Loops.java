c180d51157d193675a1b79816cf23a23bcdb511a
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-13 18:47:31 +0200

    updated the loops folder

--- java-mini-projects/Loops/src/Loops.java
+++ java-mini-projects/Loops/src/Loops.java
@@ -6,10 +6,6 @@ public class Loops {
             System.out.println("Currently i is: " + i);
         }
 
-        //while loops in java
-        //int x = 1;
-        //while (x <= 5) {
-            //System.out.println("While loop: i = " + x);
-        //}
+      
     }
 }
---
+++ java-mini-projects/Loops/src/WhileLoops.java
@@ -0,0 +1,5 @@
+public class WhileLoops {
+    public static void main(String[] args){
+        System.out.println("While loops file");
+    }
+}
