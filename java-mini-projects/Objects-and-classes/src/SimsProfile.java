8152b1eafbe39778b0902d3bbe9e7dd3eea50d32
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-13 21:19:06 +0200

    updated the simsprofile

--- java-mini-projects/Objects-and-classes/src/SimsProfile.java
+++ java-mini-projects/Objects-and-classes/src/SimsProfile.java
@@ -1,3 +1,18 @@
+class Sim{
+    //instance variables
+    String name;
+    String hairColor;
+    Double height;
+    int age;
+    String boyfriend;
+
+    //method
+    void profile(){
+        System.out.println("This is the your Sim Profile: " + name + ", they are currently " + age + " years old. The sim will have " + hairColor + " hair color, they're gonna be " + height + "cm in height and they're currently dating " + boyfriend);
+    }
+}
+
+
 public class SimsProfile {
     public static void main(String[] args){
         System.out.println("Sims Tester class");
