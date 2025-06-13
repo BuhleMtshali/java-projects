710aa38311bfaa2899f2720d5f104a67e5af0048
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-13 21:46:38 +0200

    created the sim profile

--- java-mini-projects/Objects-and-classes/src/SimsProfile.java
+++ java-mini-projects/Objects-and-classes/src/SimsProfile.java
@@ -1,47 +1,58 @@
 import java.util.Scanner;
-class Sim{
-    //instance variables
+
+class Sim {
     String name;
     String hairColor;
     Double height;
     int age;
     String boyfriend;
 
-    //method
     void profile(){
-        System.out.println("This is your Sim Profile: " + "Their name is " + name + ", they are currently " + age + " years old. The sim will have " + hairColor + " hair, they're gonna be " + height + "cm in height and they're currently dating " + boyfriend);
+        System.out.println("\n🎮 This is your Sim Profile:");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age + " years old");
+        System.out.println("Hair Color: " + hairColor);
+        System.out.println("Height: " + height + "cm");
+        System.out.println("Dating: " + boyfriend);
+        System.out.println("-------------------------------");
     }
 }
 
-
 public class SimsProfile {
     public static void main(String[] args){
-       Scanner input = new Scanner(System.in);
-        
-       Sim mySim = new Sim();
+        Scanner input = new Scanner(System.in);
+        String continueChoice = "yes";
 
-       //getting the sim name
-       System.out.println("---- Welcome to your Sim Creator Profile💁🏼‍♀️ ----");
-       System.out.print("Enter the name for your sim: ");
-       mySim.name = input.nextLine();
+        System.out.println("---- Welcome to your Sim Creator Profile 💁🏼‍♀️ ----");
 
-       System.out.print("Enter the hair color of your sim: ");
-       mySim.hairColor = input.nextLine();
+        while (continueChoice.equalsIgnoreCase("yes")) {
+            Sim mySim = new Sim();
 
-       System.out.print("Enter the height of your sim: ");
-       mySim.height = input.nextDouble();
-       input.nextLine();
+            System.out.print("\nEnter the name for your sim: ");
+            mySim.name = input.nextLine();
 
-       System.out.print("Enter the age of your sim: ");
-       mySim.age = input.nextInt();
+            System.out.print("Enter the hair color of your sim: ");
+            mySim.hairColor = input.nextLine();
 
-       System.out.print("Enter your Sim's boyfriend's name: ");
-       mySim.boyfriend = input.nextLine();
-       input.nextLine();
+            System.out.print("Enter the height of your sim (in cm): ");
+            mySim.height = input.nextDouble();
+            input.nextLine(); // clear newline
 
-       mySim.profile();
+            System.out.print("Enter your Sim's boyfriend's name: ");
+            mySim.boyfriend = input.nextLine();
 
+            System.out.print("Enter the age of your sim: ");
+            mySim.age = input.nextInt();
+            input.nextLine(); // clear newline
 
-       input.close();
+            mySim.profile();
+
+            // Ask user if they want to create another Sim
+            System.out.print("Do you want to create another Sim? (yes/no): ");
+            continueChoice = input.nextLine();
+        }
+
+        System.out.println("\nThanks for playing Sim Creator™. Buhle out! 💅🏾💻");
+        input.close();
     }
 }
