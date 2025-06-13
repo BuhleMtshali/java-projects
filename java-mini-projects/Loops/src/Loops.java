ea04bdd5fdaa2ed5e764503d18f92c96924e088c
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-13 18:55:16 +0200

    updated the loops folder

--- java-mini-projects/Loops/src/Loops.java
+++ java-mini-projects/Loops/src/Loops.java
@@ -3,7 +3,11 @@ public class Loops {
     public static void main(String[] args){
         //for loops in java
         for(int i = 0; i < 6; i++){
-            System.out.println("Currently i is: " + i);
+            System.out.println("Starting at 0, Currently i is: " + i);
+        }
+
+        for(int i = 1; i <= 3; i++){
+            System.out.println("Starting 1, i is currently: " + i);
         }
 
       
