7bbf55be3dc7c5f634fe2bdbb1a544ff274a46a8
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-19 21:19:27 +0200

    updated the People Array

--- java-mini-projects/arrays-java/PeopleArray.java
+++ java-mini-projects/arrays-java/PeopleArray.java
@@ -11,6 +11,29 @@ class Person {
 
 public class PeopleArray {
     public static void main(String[] args){
-        System.out.println("Practice");
+
+        //creating the array of people objects
+        Person[] myPersons = new Person[3];
+
+
+        //creating individual person
+        myPersons[0] = new Person();
+        myPersons[1] = new Person();
+        myPersons[2] = new Person();
+
+        //assigning values to the objects
+        myPersons[0].name = "Zack";
+        myPersons[1].name = "Linda";
+        myPersons[2].name = "Billy";
+
+        myPersons[0].age = 18;
+        myPersons[1].age = 24;
+        myPersons[2].age = 30;
+
+        //creating a loop to invoke the method
+        for (int i = 0; i < myPersons.length; i++){
+            myPersons[i].intro();
+        }
+
     }
 }
