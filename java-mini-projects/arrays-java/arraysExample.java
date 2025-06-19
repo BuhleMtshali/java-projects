9d300883df78a20b1deb569d8ff1fa7a36260da1
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-19 19:02:48 +0200

    updated the array example file

--- java-mini-projects/arrays-java/arraysExample.java
+++ java-mini-projects/arrays-java/arraysExample.java
@@ -1,6 +1,12 @@
 public class arraysExample {
-
     public static void main(String[] args){
-        System.out.println("Hello arrays");
+        int[] numbers = new int[5];
+        numbers[0] = 10;
+        numbers[1] = 20;
+        numbers[2] = 24;
+        numbers[3] = 85;
+        numbers[4] = 12;
+
+        System.out.println("Here is my first list: " + numbers);
     }
 }
