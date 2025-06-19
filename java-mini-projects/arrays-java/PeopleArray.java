e907475e49a6651949fdb77f0791af68e4579257
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-19 21:04:32 +0200

    updated the PeopleArray

---
+++ java-mini-projects/arrays-java/PeopleArray.java
@@ -0,0 +1,16 @@
+//class blueprint
+class Person {
+    String name;
+    int age;
+
+    void intro(){
+        System.out.println("Hi, my name is " + name + " and I'm currently " + age + " years old!");
+    }
+}
+
+
+public class PeopleArray {
+    public static void main(String[] args){
+        System.out.println("Practice");
+    }
+}
