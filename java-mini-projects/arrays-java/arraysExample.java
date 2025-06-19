90fd0d59fdc475c7ea739d49ceb5c98fc86e8f71
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-19 19:44:09 +0200

    updated the array example file by practicing printing out a list

--- java-mini-projects/arrays-java/arraysExample.java
+++ java-mini-projects/arrays-java/arraysExample.java
@@ -1,3 +1,4 @@
+
 public class arraysExample {
     public static void main(String[] args){
         int[] numbers = new int[5];
@@ -7,6 +8,9 @@ public class arraysExample {
         numbers[3] = 85;
         numbers[4] = 12;
 
-        System.out.println("Here is my first list: " + numbers);
+        System.out.println("\n===Here is how we print an array with a for loop===");
+        for (int i = 0; i < numbers.length; i++){
+            System.out.println("Here is the value at index: " + i);
+        }
     }
 }
