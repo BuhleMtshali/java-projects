625ccf1f487871be1a746ba579bb3e7f8ff31695
Buhle <mtshalizanobuhle7@gmail.com>
2025-06-24 19:29:36 +0200

    updated the catobject

--- java-mini-projects/arrays-java/CatObject.java
+++ java-mini-projects/arrays-java/CatObject.java
@@ -1,3 +1,18 @@
+class Cat{
+    String name;
+    String color;
+
+    void meow(){
+        System.out.println(name + " is my " + color + " colored cat!");
+    };
+    
+}
+
 public class CatObject {
+    public static void main(String[] args){
+        Cat[] myCat = new Cat[4];
+
+        
+    }
     
 }
