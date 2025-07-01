9de2e4d74bee421d168f65723517826c519ebb6d
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-01 20:29:33 +0200

    created the dogInstance file

--- java-mini-projects/Objects-and-classes/src/DogInstances.java
+++ java-mini-projects/Objects-and-classes/src/DogInstances.java
@@ -1,3 +1,17 @@
+class DogInstance {
+    int age;
+    String name;
+
+    void barkNoice(){
+        if (age > 5){
+            System.out.println(name + " is a " + age + " years old dog, and he makes a woof woof!! noice!");
+        } else if(age <= 3){
+            System.out.println(name + " is a " + age + " years old dog and he makes a Yipi Yipi!! noice");
+        }
+    }
+    
+}
+
 public class DogInstances {
     public static void main(String[] args){
         System.out.println("Hello instances");
