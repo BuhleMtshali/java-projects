99978f7953e1c61d85e16756587b893430e38066
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-01 20:32:15 +0200

    imported the scanner object

--- java-mini-projects/Objects-and-classes/src/DogInstances.java
+++ java-mini-projects/Objects-and-classes/src/DogInstances.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 class DogInstance {
     int age;
     String name;
@@ -7,6 +8,8 @@ class DogInstance {
             System.out.println(name + " is a " + age + " years old dog, and he makes a woof woof!! noice!");
         } else if(age <= 3){
             System.out.println(name + " is a " + age + " years old dog and he makes a Yipi Yipi!! noice");
+        } else {
+            System.out.println(name + " is a " + age + " years dog and he is quite normal!!");
         }
     }
     
@@ -14,6 +17,6 @@ class DogInstance {
 
 public class DogInstances {
     public static void main(String[] args){
-        System.out.println("Hello instances");
+        
     }
 }
