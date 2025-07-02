b5e0036a3ebba1a57f8caee084280521eb13c61b
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-02 20:49:52 +0200

    updated the catinstance

--- java-mini-projects/Objects-and-classes/src/CatInstance.java
+++ java-mini-projects/Objects-and-classes/src/CatInstance.java
@@ -1,3 +1,14 @@
+class Cat{
+    String name;
+
+    void meow(int catCry){
+        while (catCry > 0) {
+            System.out.print(name + " says meow!!!!");
+            catCry = catCry - 1;
+        }
+    }
+}
+
 public class CatInstance {
     public static void main(String[] args){
         System.out.println("hello");
