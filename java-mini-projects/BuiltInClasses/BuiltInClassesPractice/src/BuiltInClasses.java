3e9e80ab99aca91f42624e3e0fc36e6534b94e41
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-12 16:43:22 +0200

    practiced with the math  class

--- java-mini-projects/BuiltInClasses/BuiltInClassesPractice/src/BuiltInClasses.java
+++ java-mini-projects/BuiltInClasses/BuiltInClassesPractice/src/BuiltInClasses.java
@@ -3,5 +3,9 @@ public class BuiltInClasses {
         //1. the String Built in Class
         String name = "Buhle";
         System.out.println("Hi " + name + "!!");
+
+        //using the Math class
+        double squareRoot = Math.sqrt(144);
+        System.out.println("The squar root of 144 is: " + squareRoot);
     }
 }
