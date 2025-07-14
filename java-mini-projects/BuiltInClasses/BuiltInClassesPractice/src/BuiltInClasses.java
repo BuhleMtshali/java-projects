814f5229c2a4b2826d3403db313268e57ba70f9c
Buhle <mtshalizanobuhle7@gmail.com>
2025-07-14 18:59:23 +0200

    added the thirs item to the array

--- java-mini-projects/BuiltInClasses/BuiltInClassesPractice/src/BuiltInClasses.java
+++ java-mini-projects/BuiltInClasses/BuiltInClassesPractice/src/BuiltInClasses.java
@@ -24,6 +24,7 @@ public class BuiltInClasses {
         System.out.println(groceries);
         System.out.println("Enter the third item: ");
         String item3 = input.nextLine();
+        groceries.add(item3);
 
         input.close();
     }
